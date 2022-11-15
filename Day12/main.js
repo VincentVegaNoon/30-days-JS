8be14635c8d5af8c4c8ed795e9212966017a56c8
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2022-11-15 19:10:40 +0100

    js

--- Day12/main.js
+++ Day12/main.js
@@ -9,3 +9,37 @@ let totalIncome = salaryAnnual + bonus + coursesAnnual
 console.log(salaryAnnual)
 
 //2 The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+const points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+const sortedPoints = points.sort(function (a, b){
+    return a - b
+})
+console.log(sortedPoints)
+function getDiffrence(x, z) {
+    let a = parseInt(x)
+    let b = parseInt(z)
+    if(a < 0 || b < 0) {
+        let c = a + b
+        let sum = b + c
+        console.log(sum)
+    }
+    else {
+       return a - b
+    }
+}
+getDiffrence(sortedPoints[0], sortedPoints[sortedPoints.length - 1])
+
+//3 Write a pattern which identify if a string is a valid JavaScript variable
+
+function isValidVariable(variable) {
+    let clean = /[^0-9][%@&#?!;-]/g
+    let variableCopy = variable.replace(clean, '')
+    if (variable.length > variableCopy.length) {
+        console.log(false)
+    } else {
+        console.log(true)
+    }
+}
+isValidVariable('1first_name')
+
+// Exercises: Level 2
+paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
