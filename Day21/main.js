887c915171db349c3f758634c99bdd2eb6a1408c
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2022-11-25 15:18:54 +0100

    js

--- Day21/main.js
+++ Day21/main.js
@@ -24,4 +24,25 @@ allPs[2].setAttribute('id', 'third-paragraph')
 
 allPs[0].classList.add('paragraph', 'first-paragraph')
 
-//1
+// Exercise: Level 2
+
+//1 Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+allPs[0].style.color = 'white'
+allPs[0].style.backgroundColor = 'black'
+allPs[0].style.border = '2px solid red'
+allPs[0].style.fontSize= '34px'
+allPs[0].style.fontFamily= 'Arial'
+
+//2 Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
+allPs.forEach((item, i) => {
+    if (i % 2 === 0) {
+        item.style.backgroundColor = 'green'
+    } else {
+        item.style.backgroundColor = 'red'
+    }
+})
+//3 Set text content, id and class to each paragraph
+allPs[0].textContent = 'First paragraph'
+allPs[1].textContent = 'Second paragraph'
+allPs[2].textContent = 'Third paragraph'
+allPs[3].textContent = 'Fourth paragraph'
